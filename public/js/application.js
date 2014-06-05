5c8aff18085c0ae0d07b38200b04dd089cb3f26a
Corey White <smortopahri@gmail.com>
2014-06-05 10:37:48 -0400

    added grid and popup

--- public/js/application.js
+++ public/js/application.js
@@ -56,13 +56,57 @@ $(function() {
 		// userMarker = L.marker([51.45, 30.050], { icon: redIcon });
 
 		// load leaflet map
-		map = L.map('map');
+		map = L.map('map').setView([35.843768,-78.6450559], 11);
+		
+		function setGridStyle(feature){
+			switch (feature.properties.STATUS){
+				case 'INCOMPLETE': return {color: '#FF0000', fillColor: '#FF4040', fillOpacity: 0.5, "weight": .2, };
+				case 'COMPLETE': return {color: '#9FEE00', fillColor: '#B9F73E', fillOpacity: 0.5, "weight": .2, };
+				case 'INPROGRESS': return {color: '#009999', fillColor: '#33CCCC', fillOpacity: 0.5, "weight": .2, };
+			}
+		}
+
+		var buttonGroup = $('<div id="buttons" class="btn-group"></div>');
+		popupContent = {
+  	 		'COMPLETE' : '<button type="button" class="btn btn-success">COMPLETE</button>',
+  	 		'INPROGRESS' : '<button type="button" class="btn btn-info">INPROGRESS</button>',
+  	 		'INCOMPLETE' : '<button type="button" class="btn btn-danger">INCOMPLETE</button>'
+  		};
+  		
+		var popup = '<div id="buttons" class="btn-group">'+ popupContent.COMPLETE + popupContent.INPROGRESS + popupContent.INCOMPLETE + '</div>';
+  		// for (var each in popupContent){
+  			
+  		// 	$('#buttons', buttonGroup).append(popupContent[each]);
+  		// };
+  		
+  		// var str = buttonGroup.prop('innerHTML');
+
+
+		function gridAction(feature, layer){
+
+			// var popup = L.popup().setContent(str);
+			layer.bindPopup(popup);
+
+
+		}
+
+
+
+		var grid = L.esri.featureLayer('http://mapstest.raleighnc.gov/arcgis/rest/services/PublicUtility/ValveCrewTracking/MapServer/1',
+            {
+            	onEachFeature: gridAction,
+            	style: setGridStyle
+            }).addTo(map);
 
 		// leaflet API key tiler
-		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', { maxZoom: 18, detectRetina: true }).addTo(map);
+		//L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', { maxZoom: 18, detectRetina: true }).addTo(map);
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    		maxZoom:18, detectRetina: true}).addTo(map);
+
 
 		// set map bounds
-		map.fitWorld();
+		//map.fitWorld();
 		userMarker.addTo(map);
 		userMarker.bindPopup('<p>You are there! Your ID is ' + userId + '</p>').openPopup();
 
---
+++ public/js/crews.js
@@ -0,0 +1,18 @@
+
+// function setGridStyle(feature){
+// 	switch (feature.properties.STATUS){
+// 		case 'INCOMPLETE': return {color: '#FF0000', fillColor: '#FF4040', fillOpacity: 0.5 };
+// 		case 'COMPLETE': return {color: '#9FEE00', fillColor: '#B9F73E', fillOpacity: 0.5 };
+// 		case 'INPROGRESS': return {color: '#009999', fillColor: '#33CCCC', fillOpacity: 0.5 };
+// 	}
+// }
+
+
+
+
+
+// var grid = L.esri.featureLayer('http://mapstest.raleighnc.gov/arcgis/rest/services/PublicUtility/ValveCrewTracking/MapServer/1',
+//               {
+//                 //onEachFeature: form,
+//                 style: setGridStyle
+//               }).addTo(map);
